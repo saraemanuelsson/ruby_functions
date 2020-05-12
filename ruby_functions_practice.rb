c93f7ba6105867390fefdbd3adb00ec4f2e407d1
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-12 15:26:51 +0100

    Initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,21 @@
+# TODO: write implementation here
+
+def return_five()
+    return 5
+end
+
+def return_10()
+    return 10
+end
+
+def add(first, second)
+    return first + second
+end
+
+def subtract(first, second)
+    return first - second
+end
+
+def multiply(num_1, num_2)
+    num_1 * num_2
+end
